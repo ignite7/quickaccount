9996418b2bcd98059e76a88fa697ccdbfbb6bb60
Sergio <59528625+ignite7@users.noreply.github.com>
2020-08-04 01:47:37 +0200

    Fixed does not show the window for captcha.

--- app/services.py
+++ app/services.py
@@ -75,7 +75,6 @@ class CreateAccount:
 
         else:
             options = Options()
-            options.add_argument('--headless')
             options.add_argument('--no-sandbox')
             options.add_argument('--disable-dev-shm-usage')
 
